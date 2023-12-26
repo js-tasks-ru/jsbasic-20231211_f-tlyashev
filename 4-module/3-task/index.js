4a39504bd2be27aa519c0767c6317fd94567222f
unknown <f.tlyashev@gmail.com>
2023-12-26 14:38:22 +0500

    add solution for less 4 task 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+  let tableBodyRows = table.querySelectorAll("tbody > tr");
+
+  for (let tr of tableBodyRows) {
+    let td = tr.querySelectorAll("td");
+
+    if (td[1].textContent < 18) {
+      tr.style.textDecoration = "line-through";
+    }
+
+    if (td[2].textContent === "m") {
+      tr.classList.add("male");
+    } else if (td[2].textContent === "f") {
+      tr.classList.add("female");
+    }
+
+    if (td[3].hasAttribute("data-available")) {
+      td[3].dataset.available === "true"
+        ? tr.classList.add("available")
+        : tr.classList.add("unavailable");
+    } else {
+      tr.hidden = true;
+    }
+  }
 }
